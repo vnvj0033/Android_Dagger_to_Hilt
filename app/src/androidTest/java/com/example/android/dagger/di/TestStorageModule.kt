7cbbff3809ee72317bba942574f1f8f9f64c0e06
yoosangyeob <syyoo@joara.com>
2022-09-30 18:01:48 +0900

    - fixed ui test

--- app/src/androidTest/java/com/example/android/dagger/di/TestStorageModule.kt
+++ app/src/androidTest/java/com/example/android/dagger/di/TestStorageModule.kt
@@ -16,16 +16,14 @@
 
 package com.example.android.dagger.di
 
-import com.example.android.dagger.storage.FakeStorage
-import com.example.android.dagger.storage.Storage
-import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
-// Overrides StorageModule in android tests
+@InstallIn(SingletonComponent::class)
 @Module
 abstract class TestStorageModule {
 
-    // Makes Dagger provide FakeStorage when a Storage type is requested
-    @Binds
-    abstract fun provideStorage(storage: FakeStorage): Storage
+//    @Binds
+//    abstract fun provideStorage(storage: FakeStorage): Storage
 }
